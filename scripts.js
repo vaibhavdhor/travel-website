f2909e3898937287fef2ba8f82f8552ba173db5d
vaibhavdhor <vaibhavdhor67@gmail.com>
2024-06-19 15:24:18 +0530

    add files

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Toggle Menu for Mobile
+document.querySelector('.menu-toggle').addEventListener('click', () => {
+    document.querySelector('.menu-toggle').classList.toggle('active');
+    document.querySelector('.nav-links').classList.toggle('active');
+});
